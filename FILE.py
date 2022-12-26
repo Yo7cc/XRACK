ec99f6487d3a53d682ca07e2790de24d3c0388a4
YO7 <98788798+Yo7cc@users.noreply.github.com>
2022-12-27 01:20:59 +0600

    Add files via upload

---
+++ FILE.py
@@ -0,0 +1,24 @@
+import os, platform, time
+os.system('git pull -q')
+#print('If Showing (Cookie Invailed) Try After Some Time')
+#input('Press Enter')
+#yy = input('Did You Know How to Make EAAB Token??(y/n)')
+#if yy at in 'yes Y y Yes YES':
+ #pass
+#else:
+# os.system('xdg-open https://www.facebook.com/100074059501726/posts/150776190734364/?app=fbl')
+uu = os.popen('play').read()
+
+if 'Usage summary' in uu:
+ pass
+else:
+ print(uu)
+ os.system('pkg install sox -y')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('64BIT FOUND');time.sleep(1)
+ import FILE64
+elif bit == '32bit':
+ print('32BIT FOUND');time.sleep(1)
+ import FILE32
